795823675f27afef452d3b91313e4c09421798b4
Cole J. Dermott <92142700+cdermott7@users.noreply.github.com>
2024-12-20 18:38:40 -0500

    Initial commit

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -23,7 +23,7 @@ export function Navbar() {
         <Link href="/" className="flex items-center space-x-2">
           <div className="flex items-center gap-2">
             <Image
-              src="/dbs-logo.png"
+              src="/dal-bcs.png"
               alt="Dalhousie Blockchain Society"
               width={32}
               height={32}
